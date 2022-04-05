f450455f5a5a4641257e3083f410506d61f44bd2
jocelyninlay <inlay.j@gmail.com>
2022-04-05 16:36:33 -0400

    adding authentication jdoser

--- questionbox/urls.py
+++ questionbox/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/auth/', include('djoser.urls')),
     path('api/auth/', include('djoser.urls.authtoken')),
+
 ]
