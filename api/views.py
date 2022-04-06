0a985ebfc7988220aff1c4bf3f5a282fb775e4a8
jocelyninlay <inlay.j@gmail.com>
2022-04-06 16:04:06 -0400

    updated serializer spelling

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from api.models import Question, Answer, User
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView, ListAPIView
-from .serializers import QuestionSerializer
+from .serializers import QuestionSerializer, QuestionAnswerSerializer
 
 class QuestionListView(ListCreateAPIView):
     queryset = Question.objects.all()
